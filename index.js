0322b57b7a4e05207d53473bb937dd5cd02a88b7
ArunGovil <a.arungovil@gmail.com>
2022-08-27 20:25:49 +0530

    get with params

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const data = require("./data/data.json");
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
+app.use("/", express.static(__dirname + "/static"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(port, () => {
-  console.log("Express Starting");
+app.get("/all", function (req, res) {
+  res.json(data);
 });
+
+app.get("/book/:id", function (req, res) {
+  const item = data.find((item) => (item.id = req.params.id));
+  res.json({ item });
+});
+
+app.listen(port, () => {});
+
+module.exports = app;
